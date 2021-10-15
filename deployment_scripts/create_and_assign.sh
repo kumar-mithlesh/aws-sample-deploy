e11106f7fbae3679e8eb516a1ed281577dea990a
mithlesh kumar <mk581999@gmail.com>
2021-10-15 20:22:00 +0530

    refactored code

--- deployment_scripts/create_and_assign.sh
+++ deployment_scripts/create_and_assign.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-
 # Move env variables
-# Note: puma_config file on server
-mkdir /home/ubuntu/aws-sample-deploy/tmp/pids
-mkdir /home/ubuntu/aws-sample-deploy/tmp/sockets
-
 # Assign permissions
 sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy
+
+# Note: puma_config file on server
+sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/pids
+sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/sockets
+
 sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy/tmp/pids/
