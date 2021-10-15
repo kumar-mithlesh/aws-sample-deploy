25e530bec268b15574b07991ffedaa40703d969f
mithlesh kumar <mk581999@gmail.com>
2021-10-15 20:41:03 +0530

    refactored code

--- deployment_scripts/arrange_deployment_folders.sh
+++ deployment_scripts/arrange_deployment_folders.sh
@@ -11,9 +11,3 @@ mkdir /home/ubuntu/aws-sample-deploy
 
 # Assign permissions
 sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy
-
-# Note: puma_config file on server
-sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/pids
-sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/sockets
-
-sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy/tmp/pids/
