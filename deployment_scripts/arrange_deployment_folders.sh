eb633342f8b43c79bea8bc0e8da4b5e60dfe1fe0
mithlesh kumar <mk581999@gmail.com>
2021-10-15 20:29:23 +0530

    reafactored code

--- deployment_scripts/arrange_deployment_folders.sh
+++ deployment_scripts/arrange_deployment_folders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo rm -rf /home/ubuntu/backaws-sample-deploy
+sudo rm -rf /home/ubuntu/backup/aws-sample-deploy
 
 # Backup current deployment 
 sudo mv /home/ubuntu/aws-sample-deploy /home/ubuntu/backup/aws-sample-deploy
@@ -10,3 +10,12 @@ sudo rm -rf /home/ubuntu/aws-sample-deploy
 
 # Create new deployment folder
 mkdir /home/ubuntu/aws-sample-deploy
+
+# Assign permissions
+sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy
+
+# Note: puma_config file on server
+sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/pids
+sudo mkdir /home/ubuntu/aws-sample-deploy/tmp/sockets
+
+sudo chown ubuntu:ubuntu -R /home/ubuntu/aws-sample-deploy/tmp/pids/
